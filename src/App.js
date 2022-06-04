462ac4bb3f469f96447396c10f8dc448fdaaa19b
Pham Thanh Long <pham.thanh.long@sun-asterisk.com>
2022-06-04 23:07:37 +0700

    long_blog_main_fix

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import './App.css';
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+import Create from './components/Create'
+import BlogDetail from './components/BlogDetail'
+import NotFound from './components/NotFound';
+import BlogTable from './components/BlogTable';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Navbar/>
+        <div className="content">
+          <Switch>
+            <Route exact path="/" >
+              <Home />
+            </Route>
+            <Route path="/create" >
+              <Create />
+            </Route>
+            <Route path="/blogs/:id" >
+              <BlogDetail />
+            </Route>
+            <Route path="/table">
+              <BlogTable />
+            </Route>
+            <Route path="*">
+              <NotFound />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
