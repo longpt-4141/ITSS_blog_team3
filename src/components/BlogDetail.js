51263996a7ab8660a69f7f91f7cba63fc1694578
Pham Thanh Long <pham.thanh.long@sun-asterisk.com>
2022-06-05 23:25:51 +0700

    long_edit_function

--- src/components/BlogDetail.js
+++ src/components/BlogDetail.js
@@ -1,10 +1,15 @@
-import { useHistory ,useParams } from "react-router-dom";
+import { useHistory ,useParams, Link} from "react-router-dom";
+import {useState, useEffect} from "react"
 import useFetch from './useFetch'
 const BlogDetail = () => {
-
+    const [title, setTitle] = useState('');
+    const [author, setAuthor] = useState('');
+    const [body, setBody] = useState('');
     const {id} = useParams();
     const { data:blog, error, isPending } = useFetch('http://localhost:8000/blogs/' + id);
     const history = useHistory();
+
+    
 
     const handleDelete = () => {
         fetch('http://localhost:8000/blogs/' + blog.id, { 
@@ -13,6 +18,19 @@ const BlogDetail = () => {
             history.push('/');
         })
     }
+
+    const handleEdit = () => {
+        const blogs = {id, title, body, author}
+        fetch('http://localhost:8000/blogs/' + id, {
+            method: 'PUT',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(blogs)
+        }).then(() => {
+            console.log(blogs)
+            history.go(-1);
+        })
+    }       
+
     return ( 
 
         <div className="blog-details">
@@ -23,6 +41,14 @@ const BlogDetail = () => {
                     <h2>{blog.title}</h2>
                     <p>Written by {blog.author}</p>
                     <div>{blog.body}</div>
+                    <Link 
+                        to={`/edit/${blog.id}`}
+                        style = {{
+                        color: "#fff",
+                        backgroundColor: '#f1356d',
+                        borderRadius: '8px'
+                        }}
+                    >Edit</Link>
                     <button onClick={handleDelete} >Delete</button>
                 </article>
             )}
